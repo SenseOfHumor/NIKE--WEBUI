a02325bc4928022f675bd5209b3da1b3e240fb72
SWPNL <sd2269@njit.edu>
2024-08-16 02:14:35 -0400

    changes to the landing page

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -3,24 +3,30 @@ import { arrowRight } from "../assets/icons"
 
 const Hero = () => {
   return (
-    <section
-    id = "home"
-    className = "w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container">
+<section
+      id='home'
+      className='w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container'
+    >
+      <div className='relative xl:w-2/5 flex flex-col justify-center items-start w-full  max-xl:padding-x pt-28'>
+        <p className='text-xl font-montserrat text-coral-red'>
+          Our Summer collections
+        </p>
 
-      <div className = "relative xl:w-2/5 flex flex-col justify-center intems-start w-full max-xl:padding-x pt-28">
-        <p>Our Summer Collection</p>
-        <h1>
-          <span>The New Arrivals</span>
-          <br></br>
-          <span>Nike</span>
-          Shoes
+        <h1 className='mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold'>
+          <span className='xl:bg-white xl:whitespace-nowrap relative z-10 pr-10'>
+            The New Arrival
+          </span>
+          <br />
+          <span className='text-coral-red inline-block mt-3'>Nike</span> Shoes
         </h1>
-        <p>Discover stylish Nike arrivals, quality comfort, and innovation for your active life.</p>
+        <p className='font-montserrat text-slate-gray text-lg leading-8 mt-6 mb-14 sm:max-w-sm'>
+          Discover stylish Nike arrivals, quality comfort, and innovation for
+          your active life.
+        </p>
 
-        <Button label = "Shop Now" iconURL = {arrowRight}/>
+        <Button label='Shop now' iconURL={arrowRight} />
       </div>
-
-    </section>
+      </section>
   )
 }
 
